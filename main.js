33c796257f8a33fbe19cc0e3e115a21436b9e544
kiennt28 <trungkien28.dev@gmail.com>
2021-10-12 17:25:32 +0700

    f

---
+++ main.js
@@ -0,0 +1,18 @@
+// var entry = document.getElementById("entry");
+
+// entry.addEventListener("click", displayDetails);
+
+// function displayDetails() {
+//   var number = document.getElementById("number").value;
+//   if (number.value.length == 0) {
+//     alert("Nhập lại");
+//     return false;
+//   } else if (isNaN(number.value)) {
+//     alert("Nhập lại");
+//     return false;
+//   } else {
+//     console.log(number.value);
+//     return true;
+//   }
+// console.log(1);
+// }
